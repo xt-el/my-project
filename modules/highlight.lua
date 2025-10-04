454684a7b3879de9ce3cb973982d2c17354611dd
xt-el <notartem1@gmail.com>
2025-10-04 17:25:04 -0300

    Update highlight.lua

--- modules/highlight.lua
+++ modules/highlight.lua
@@ -1,1 +1,26 @@
+local M = {}
+M.name = "highlight"
+M.version = "1.0.0"
 
+function M.init(api)
+    local Players = api.Services.Players
+    local lp = Players.LocalPlayer
+    local highlight = Instance.new("Highlight")
+    highlight.Parent = lp
+    highlight.FillTransparency = 1
+    highlight.OutlineTransparency = 1
+
+    api.lightNPC = function(adornee, color)
+        task.spawn(function()
+            highlight.Adornee = adornee
+            highlight.OutlineColor = color
+            api.Services.TweenService:Create(highlight, api.config.fast, {OutlineTransparency = 0}):Play()
+            task.wait(0.5)
+            api.Services.TweenService:Create(highlight, api.config.fast, {OutlineTransparency = 1}):Play()
+        end)
+    end
+
+    api.print("highlight loaded")
+end
+
+return M
