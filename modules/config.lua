b30f0bd30710c70f9ecc8a0488e13f4dc92a9a0f
xt-el <notartem1@gmail.com>
2025-10-04 17:24:36 -0300

    Update config.lua

--- modules/config.lua
+++ modules/config.lua
@@ -1,1 +1,15 @@
+local M = {}
+M.name = "config"
+M.version = "1.0.0"
 
+function M.init(api)
+    api.config = {
+        rad = 100,
+        selectionRange = 100,
+        autoControlRange = 20,
+        fast = TweenInfo.new(0.3, Enum.EasingStyle.Exponential)
+    }
+    api.print("config loaded")
+end
+
+return M
